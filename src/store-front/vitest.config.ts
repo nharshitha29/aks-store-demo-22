c79e21cbb6d0c6d66653e950b5e8c5fcd8fa62ce
nharshitha29 <nharshitha29@gmail.com>
2025-09-23 15:30:07 +0530

    Update vitest.config.ts

--- src/store-front/vitest.config.ts
+++ src/store-front/vitest.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
+    globals: true,
     environment: 'jsdom',
-    exclude: ['e2e/**', 'node_modules/**'],
-  }
+  },
 })

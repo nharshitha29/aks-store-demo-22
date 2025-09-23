2acd892885c4ce5ba02945712d3c3ab2d9dce0b2
nharshitha29 <nharshitha29@gmail.com>
2025-09-23 16:03:19 +0530

    Update TopNav.vue

--- src/store-front/src/components/TopNav.vue
+++ src/store-front/src/components/TopNav.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref } from "vue"
 import { useCartStore } from "@/stores"
 
 const cartStore = useCartStore()
